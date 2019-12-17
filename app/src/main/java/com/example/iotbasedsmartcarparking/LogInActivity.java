e2a36f7f1f368caa8ac0a83f26b35a34e5fa65f3
Bilaleng <nicemehar24@gmail.com>
2019-12-17 19:09:58 +0500

    Bilal Work almost done login signup

--- app/src/main/java/com/example/iotbasedsmartcarparking/LogInActivity.java
+++ app/src/main/java/com/example/iotbasedsmartcarparking/LogInActivity.java
@@ -2,16 +2,43 @@ package com.example.iotbasedsmartcarparking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class LogInActivity extends AppCompatActivity {
+    Button login;
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_log_in );
+        signup= (TextView) findViewById(R.id.notreg);
+        login= (Button) findViewById(R.id.btnlogin);
 
-        //jbsfdgshjfg
-        //dbshh
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(LogInActivity.this, SignUpActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/iotbasedsmartcarparking/MainActivity.java
+++ app/src/main/java/com/example/iotbasedsmartcarparking/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.iotbasedsmartcarparking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button login,signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        login=(Button) findViewById(R.id.btnlogin1);
+        signup= (Button) findViewById(R.id.btnreg1);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(MainActivity.this,LogInActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
